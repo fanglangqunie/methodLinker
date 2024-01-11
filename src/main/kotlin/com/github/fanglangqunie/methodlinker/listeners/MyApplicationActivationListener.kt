350e79aa4d95f96bbba00a18f22005dd7f49d19c
GitHub Action <action@github.com>
2024-01-11 06:25:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/fanglangqunie/methodlinker/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/fanglangqunie/methodlinker/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fanglangqunie.methodlinker.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
